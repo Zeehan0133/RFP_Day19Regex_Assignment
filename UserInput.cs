5fa7d8ae74b0de40fc2f8e3df79a85f479954036
Zeehan0133 <zyne0133@gmail.com>
2021-10-05 18:43:43 +0530

    Declare the the inputs by get set property

---
+++ UserInput.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19_Regex_Ass
+{
+    class UserInput
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Email { get; set; }
+        public string MobileNum { get; set; }
+        public string Passwd { get; set; }
+
+
+
+    }
+}
